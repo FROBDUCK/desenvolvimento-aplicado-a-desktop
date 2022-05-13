49979d3bc43a89fedac8760ad26fcb240e3f0c6d
victor luiz almeida medeiros <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-05-13 19:51:50 -0300

    Add files via upload

---
+++ hua.java
@@ -0,0 +1,17 @@
+package ghk;
+
+public class hua {
+public static void main(String[]args) {
+int nota = 9;
+
+if (nota>=9) {
+System.out.println("a");	
+
+}else if (nota>=7) {
+	System.out.println("b");
+	
+}else{
+	System.out.println("c");
+}
+}
+}
