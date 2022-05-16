ea8c28db7d4a37aadbc3ed741cd873a9302a6834
victor luiz almeida medeiros <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-05-15 21:29:45 -0300

    Add files via upload

---
+++ estoquemedio.java
@@ -0,0 +1,13 @@
+package ghk;
+
+public class estoquemedio {
+	public static void main(String[]args) {
+    int QM = 10;
+    int QMAX = 20;
+    int estoquemedio;
+    
+    estoquemedio = (QM + QMAX)/2;
+    System.out.println(estoquemedio);
+    
+	}
+}
