8a6d9316080ff0665e49150606ab4d142a60806d
victor luiz almeida medeiros <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-05-21 00:42:04 -0300

    Add files via upload

---
+++ maça.java
@@ -0,0 +1,20 @@
+package ghk;
+
+public class maça {
+	public static void main(String[]args) {
+double preço;
+int maça = 11; 
+{
+
+if (maça >= 12) {
+	preço = 1.00;
+	System.out.println(preço);
+
+}else {
+	preço = 1.30;
+	System.out.println(preço);
+}
+
+}
+}
+}
