71715c84c8fc33749707ec4a0e3254af1ef3ba8a
victor luiz almeida medeiros <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-05-22 19:22:06 -0300

    Add files via upload

---
+++ celcius.java
@@ -0,0 +1,21 @@
+package ghk;
+
+public class celcius {
+	public static void main(String[]args) {
+		int c = 30;
+		int f;
+		{
+			f = 9 * c + 160 / 5;
+			System.out.println(f);
+		}
+	}
+
+	public static void main(String[]args) {
+		int c = 30;
+		int f;
+		{
+			f = 9 * c + 160 / 5;
+			System.out.println(f);
+		}
+	}
+}
