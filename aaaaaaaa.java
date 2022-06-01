d9ed79abe2a58860136ac3e982a522b7bc5e1e70
victor luiz almeida medeiros <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-05-31 21:47:16 -0300

    Add files via upload

---
+++ aaaaaaaa.java
@@ -0,0 +1,24 @@
+package ghk;
+
+public class aaaaaaaa {
+	public static void main(String[]args) {
+		int n1 = 2;
+		int n2 = 4;
+		int multi;
+		int div;
+		int soma;
+		int sub;
+
+	soma = (n1+ n2);
+	sub = (n1 - n2);
+	div = (n1 % n2);
+	multi = (n1 * n2);
+	
+	System.out.println(soma);
+	System.out.println(sub);
+	System.out.println(multi);
+	System.out.println(div);
+	
+	
+}
+}
