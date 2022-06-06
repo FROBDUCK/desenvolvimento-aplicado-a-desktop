bece139cf3d8316ce65d51dd8c659f1887711f14
victor luiz <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-06-05 21:51:36 -0300

    Add files via upload

---
+++ imc.java
@@ -0,0 +1,18 @@
+package imc;
+
+public class imc {
+	public static void main(String[]args) {
+	double a = 1.86;
+	double peso = 81;
+	double imc;
+	double a2;
+	
+	if(peso/(a*a)<20){
+		System.out.println("abaixo do peso");
+	}else if(peso/(a*a)<=25) {
+		System.out.println("peso normal");
+	}
+	    
+	    
+	}
+}
