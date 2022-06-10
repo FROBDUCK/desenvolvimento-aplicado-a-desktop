380ade2888308252b9e7b0a22b60654ee5a38ade
victor luiz <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-06-10 19:59:49 -0300

    Add files via upload

---
+++ um.java
@@ -0,0 +1,24 @@
+package ex3;
+
+public class um {
+public static void main(String[] args) {
+	boolean issnowing = true;
+	boolean israining = true;
+	double temperatura = 40;
+	
+	if (issnowing = false) {
+	System.out.println("vamos sair");
+	}else if (israining = false) {
+		System.out.println("vamos sair");
+	}if (temperatura > 50) {
+		System.out.println("vamos sair");
+	}else {
+		System.out.println("vamos ficar em casa");
+	}
+		
+	
+			
+	
+}
+
+}
